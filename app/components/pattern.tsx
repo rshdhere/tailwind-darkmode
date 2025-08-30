6c5040906da6e452de794ab82f945153626e0502
raashed <mohammedraashed10@gmail.com>
2025-08-30 08:21:20 +0530

    feat: added marquee and other stuff

---
+++ app/components/pattern.tsx
@@ -0,0 +1,5 @@
+export const Pattern = () => {
+  return (
+    <div className="pointer-events-none absolute inset-0 z-0 m-auto rounded-lg bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed"></div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.className} antialiased [--pattern-fg:var(--color-neutral-900)]/5`}
+        className={`${inter.className} antialiased [--pattern-fg:var(--color-neutral-950)]/[0.02] dark:[--pattern-fg:var(--color-neutral-50)]/10`}
       >
-        <main className="bg-background text-foreground h-screen">
-          {children}
-        </main>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main className="bg-background text-foreground h-screen">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
